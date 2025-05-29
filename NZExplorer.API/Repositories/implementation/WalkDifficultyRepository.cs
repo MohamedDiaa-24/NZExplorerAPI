ac51a9c14b4f4b1096e3a380dfff33fed9831500
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-05-29 23:57:57 +0300

    Add Implementation for WalkDifficultyRepository

--- NZExplorer.API/Repositories/implementation/WalkDifficultyRepository.cs
+++ NZExplorer.API/Repositories/implementation/WalkDifficultyRepository.cs
@@ -1,33 +1,63 @@
-﻿using NZExplorer.API.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using NZExplorer.API.Data;
+using NZExplorer.API.Models;
 using NZExplorer.API.Repositories.interfaces;
 
 namespace NZExplorer.API.Repositories.implementation
 {
     public class WalkDifficultyRepository : IWalkDifficultyRepository
     {
-        public Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
+        private readonly AppDBContext _dBContext;
+
+        public WalkDifficultyRepository(AppDBContext appDBContext)
         {
-            throw new NotImplementedException();
+            _dBContext = appDBContext;
         }
 
-        public Task<WalkDifficulty> DeleteAsync(Guid id)
+        public async Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
         {
-            throw new NotImplementedException();
+            walkDifficulty.Id = Guid.NewGuid();
+
+            await _dBContext.WalkDifficulty.AddAsync(walkDifficulty);
+            await _dBContext.SaveChangesAsync();
+            return walkDifficulty;
         }
 
-        public Task<IEnumerable<WalkDifficulty>> GetAllAsync()
+        public async Task<WalkDifficulty?> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var WalkDiffculty = await _dBContext.WalkDifficulty.FindAsync(id);
+
+            if (WalkDiffculty is not null)
+            {
+                _dBContext.WalkDifficulty.Remove(WalkDiffculty);
+                await _dBContext.SaveChangesAsync();
+                return WalkDiffculty;
+            }
+            return null;
         }
 
-        public Task<WalkDifficulty?> GetByIdAsync(Guid id)
+        public async Task<IEnumerable<WalkDifficulty>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _dBContext.WalkDifficulty.ToListAsync();
         }
 
-        public Task<WalkDifficulty> UpdateAsync(Guid id, WalkDifficulty walkDifficulty)
+        public async Task<WalkDifficulty?> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _dBContext.WalkDifficulty.FindAsync(id);
+        }
+
+        public async Task<WalkDifficulty?> UpdateAsync(Guid id, WalkDifficulty walkDifficulty)
+        {
+            var existingWalkDiffculty = await _dBContext.WalkDifficulty.FindAsync(id);
+
+            if (walkDifficulty is not null)
+            {
+                existingWalkDiffculty.Code = walkDifficulty.Code;
+                await _dBContext.SaveChangesAsync();
+                return existingWalkDiffculty;
+            }
+            return null;
+
         }
     }
 }
--- NZExplorer.API/Repositories/interfaces/IWalkDifficultyRepository.cs
+++ NZExplorer.API/Repositories/interfaces/IWalkDifficultyRepository.cs
@@ -7,7 +7,7 @@ namespace NZExplorer.API.Repositories.interfaces
         Task<IEnumerable<WalkDifficulty>> GetAllAsync();
         Task<WalkDifficulty?> GetByIdAsync(Guid id);
         Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty);
-        Task<WalkDifficulty> UpdateAsync(Guid id, WalkDifficulty walkDifficulty);
-        Task<WalkDifficulty> DeleteAsync(Guid id);
+        Task<WalkDifficulty?> UpdateAsync(Guid id, WalkDifficulty walkDifficulty);
+        Task<WalkDifficulty?> DeleteAsync(Guid id);
     }
 }
